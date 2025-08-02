521b444e467e523b6eb8fb5df96ad53941149d84
Harshit yadav <harshityadav5499@gmail.com>
2025-08-02 22:38:08 +0530

    added the funding of a account through airdrop and making account

---
+++ GeneratingPDA.js
@@ -0,0 +1,12 @@
+// import { Address, getProgramDerivedAddress } from "@solana/kit";
+
+// const programAddress = "11111111111111111111111111111111" as Address;
+
+// const seeds = ["helloWorld"];
+// const [pda, bump] = await getProgramDerivedAddress({
+//   programAddress,
+//   seeds
+// });
+
+// console.log(`PDA: ${pda}`);
+// console.log(`Bump: ${bump}`);
